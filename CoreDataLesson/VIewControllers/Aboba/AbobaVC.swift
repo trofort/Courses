0bc25529db0b9c8fc4519f2ed831dba42262e1e4
Lobsterinho <lobsterrinho@gmail.com>
2023-02-07 21:11:05 +0300

    Created AbobaVC

---
+++ CoreDataLesson/VIewControllers/Aboba/AbobaVC.swift
@@ -0,0 +1,19 @@
+//
+//  AbobaVC.swift
+//  CoreDataLesson
+//
+//  Created by Lobster on 7.02.23.
+//
+
+import Foundation
+import UIKit
+
+final class AbobaVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("ABOBA")
+    }
+    
+}
