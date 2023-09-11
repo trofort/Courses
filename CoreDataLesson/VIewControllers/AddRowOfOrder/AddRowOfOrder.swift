ddb9771fa5fa30383f41fef6790ad2c3d69a7d41
Elizaveta Shidlovskaya <shidlovskayaelizaveta@gmail.com>
2023-09-11 21:21:16 +0300

    Add theBestButton

--- CoreDataLesson/VIewControllers/AddRowOfOrder/AddRowOfOrder.swift
+++ CoreDataLesson/VIewControllers/AddRowOfOrder/AddRowOfOrder.swift
@@ -17,6 +17,8 @@ final class AddRowOfOrder: UIViewController, ServicesListVCDelegate {
     @IBOutlet private weak var priceTextField: UITextField!
     @IBOutlet private weak var serviceButton: UIButton!
     
+    @IBOutlet private var theBestButton: UIButton!
+    
     weak var delegate: AddRowOfOrderDelegate?
     
     private var selectedService: Service? {
